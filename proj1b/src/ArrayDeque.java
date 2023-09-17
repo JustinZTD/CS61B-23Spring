b863b6cc236a73296c33158ac7fd68c225dff79d
Justin <zhangtianda90@gmail.com>
2023-09-17 22:22:08 +0900

    complete project 1b

---
+++ proj1b/src/ArrayDeque.java
@@ -0,0 +1,123 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque<T> implements Deque<T>{
+    private T[] items;
+    private int size;
+    private int first;
+    private int last;
+    private int capacity;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        first = 0;
+        last = 0;
+        size = 0;
+        capacity = 8;
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> ad = new ArrayDeque<>();
+        for (int i = 0; i < 16; i++) {
+            ad.addLast(i);
+        }
+        ad.addLast(16);
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if (items[first] == null) {
+            items[first] = x;
+            size++;
+            return;
+        }
+        int newFirst = (first + capacity - 1) % capacity;
+        if (items[newFirst] != null) {
+            resize(size * 2);
+            newFirst = (first + capacity - 1) % capacity;
+        }
+        items[newFirst] = x;
+        first = newFirst;
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        if (items[last] == null) {
+            items[last] = x;
+            size++;
+            return;
+        }
+        int newLast = (last + capacity + 1) % capacity;
+        if (items[newLast] != null) {
+            resize(size * 2);
+            newLast = (last + 1) % capacity;
+        }
+        items[newLast] = x;
+        last = newLast;
+        size++;
+    }
+
+    private void resize(int capacity) {
+        this.capacity = capacity;
+        T[] newAd = (T[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            newAd[i] = items[(first + i) % items.length];
+        }
+        first = 0;
+        last = size - 1;
+        items = newAd;
+    }
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            returnList.add(items[(first + i) % capacity]);
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        int oldFirst = first;
+        T itemRemoved = items[oldFirst];
+        items[oldFirst] = null;
+        first = (first + 1) % capacity;
+        size--;
+        if ((double) size / capacity < 0.25 && capacity > 16) {
+            resize(capacity / 2);
+        }
+        return itemRemoved;
+    }
+
+    @Override
+    public T removeLast() {
+        int oldLast = last;
+        T itemRemoved = items[oldLast];
+        items[oldLast] = null;
+        last = (last - 1 + capacity) % capacity;
+        size--;
+        if ((double) size / capacity < 0.25 && capacity > 16) {
+            resize(capacity / 2);
+        }
+        return itemRemoved;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        }
+        return items[(first + index) % capacity];
+    }
+}
--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -1,8 +1,108 @@
 import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
+    @Test
+    public void addFirst_singleItem() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(1);
 
+        assertThat(deque.toList()).containsExactly(1);
+    }
+
+    @Test
+    public void addFirst_multipleItems() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(2);
+        deque.addFirst(1);
+
+        assertThat(deque.toList()).containsExactly(1, 2).inOrder();
+    }
+
+    @Test
+    public void addLast_singleItem() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addLast(1);
+
+        assertThat(deque.toList()).containsExactly(1);
+    }
+
+    @Test
+    public void addLast_multipleItems() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addLast(1);
+        deque.addLast(2);
+
+        assertThat(deque.toList()).containsExactly(1, 2).inOrder();
+    }
+
+    @Test
+    public void resize_whileAdding() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < 16; i++) {
+            deque.addLast(i);
+        }
+
+        deque.addLast(16); // this should trigger resize
+
+        assertThat(deque.toList()).hasSize(17);
+        assertThat(deque.get(16)).isEqualTo(16);
+    }
+
+    @Test
+    public void removeFirst() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(2);
+        deque.addFirst(1);
+        int removedItem = deque.removeFirst();
+
+        assertThat(removedItem).isEqualTo(1);
+        assertThat(deque.toList()).containsExactly(2);
+    }
+
+    @Test
+    public void removeLast() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addLast(1);
+        deque.addLast(2);
+        int removedItem = deque.removeLast();
+
+        assertThat(removedItem).isEqualTo(2);
+        assertThat(deque.toList()).containsExactly(1);
+    }
+
+    @Test
+    public void resize_whileRemoving() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < 20; i++) {
+            deque.addLast(i);
+        }
+        for (int i = 0; i < 15; i++) {
+            deque.removeFirst();
+        }
+
+        assertThat(deque.toList()).hasSize(5);
+    }
+
+    @Test
+    public void isEmpty() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        assertThat(deque.isEmpty()).isTrue();
+
+        deque.addLast(1);
+        assertThat(deque.isEmpty()).isFalse();
+
+        deque.removeLast();
+        assertThat(deque.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void get_outOfBounds() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addLast(1);
+
+        assertThat(deque.get(1)).isNull();
+        assertThat(deque.get(-1)).isNull();
+    }
 }
